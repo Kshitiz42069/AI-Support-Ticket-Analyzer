5147bcfcb8f2b98b90b31ee8a18fd5e03ac2e1d9
KSHITIZ RAI <kshitizroy836@gmail.com>
2026-03-08 14:33:13 +0530

    backend init and tickets CRUD

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import tickets
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins =["http://localhost:5173"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+app.include_router(tickets.router, prefix='/tickets', tags=["Tickets"])
+
+@app.get('/')
+def root():
+    return {"message":"Backend is running......"}
