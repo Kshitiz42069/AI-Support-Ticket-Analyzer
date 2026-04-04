9567e77f337edfdeea5b9458887ac21db1eae0e0
KSHITIZ RAI <kshitizroy836@gmail.com>
2026-04-04 14:35:24 +0530

    bcrypt issues fixes

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hashPassword(password:str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verifyPassword(plainPassword, hashPassword):
-    return pwd_context.verify(plainPassword,hashPassword)
+    return pwd_context.verify(plainPassword[:72],hashPassword)
